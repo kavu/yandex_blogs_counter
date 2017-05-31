2d9c68696547fd0ce09026ce8ea228fbe1753781
Max Riveiro <kavu13@gmail.com>
2017-05-31 23:40:53 +0300

    Handle paramateres properly
    Signed-off-by: Max Riveiro <kavu13@gmail.com>

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject solar "0.1.0-SNAPSHOT"
             :dependencies [[org.clojure/clojure "1.8.0"]
                            [org.clojure/data.json "0.2.6"]
-                           [http-kit "2.2.0"]]
+                           [http-kit "2.2.0"]
+                           [ring "1.6.1"]]
             :profiles {:dev {:dependencies [[proto-repl "0.3.1"]]}
                        :uberjar {:aot :all}}
             :main ^:skip-aot solar.core
--- src/solar/core.clj
+++ src/solar/core.clj
@@ -1,14 +1,23 @@
 (ns solar.core
-  (:require [org.httpkit.server :as http]
-            [clojure.data.json :as json]))
+  (:use
+    [clojure.data.json :as json]
+    [org.httpkit.server :only [run-server] :as http]
+    (ring.middleware
+      [keyword-params :only [wrap-keyword-params]]
+      [params :only [wrap-params]])))
 
 (defn app [req]
   {:status  200
    :headers {"Content-Type" "application/json; charset=utf-8"}
-   :body    (json/write-str {"habr.ru" 13})})
+   :body
+    (let [q (-> req :params :q)]
+      (json/write-str {:resp q}))})
 
 (defn -main
   [& args]
   (println "Starting Server!")
-  (http/run-server app
-                   {:port 8080}))
+  (http/run-server
+    (-> app
+      wrap-keyword-params
+      wrap-params)
+    {:port 8080}))
