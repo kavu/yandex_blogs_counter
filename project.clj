0b2011e1dab00a3dab9d1b1a7fd684aeb7acc24a
Max Riveiro <kavu13@gmail.com>
2017-06-01 23:13:33 +0300

    Add Claypool for easy thread usage
    Signed-off-by: Max Riveiro <kavu13@gmail.com>

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
             :dependencies [[org.clojure/clojure "1.8.0"]
                            [org.clojure/data.json "0.2.6"]
                            [http-kit "2.2.0"]
-                           [ring/ring-defaults "0.3.0"]]
+                           [ring/ring-defaults "0.3.0"]
+                           [com.climate/claypoole "1.1.4"]]
             :profiles {:dev {:dependencies [[proto-repl "0.3.1"]]}
                        :uberjar {:aot :all}}
             :main ^:skip-aot solar.core
--- src/solar/core.clj
+++ src/solar/core.clj
@@ -1,24 +1,44 @@
 (ns solar.core
-  (:use
+  (:require
    [clojure.data.json :only [write-str] :as json]
    [org.httpkit.server :only [run-server] :as http]
-   [ring.middleware.defaults :only [wrap-defaults api-defaults]]))
+   [ring.middleware.defaults :only [wrap-defaults api-defaults] :as rm]
+   [com.climate.claypoole :only [threadpool ncpus pmap] :as cp]))
+
+(defonce yandex-blogs-rss
+  "https://yandex.ru/blogs/rss/search?text=%s&ft=blog,comments&numdoc=10")
 
 (defonce app-content-type
   {"Content-Type" "application/json; charset=utf-8"})
 
-(defonce body-template
+(defonce response-template
   {:status  200
    :headers app-content-type
    :body nil})
 
-(defn app [req]
-  (assoc body-template :body
-         (let [q (-> req :params :q)]
-           (json/write-str {:resp q}))))
+; Yes, we'll have one global threadpool for all requests
+(def pool
+  (cp/threadpool (cp/ncpus)))
+
+; Our processing entry function
+(defn search
+  [data]
+  {:query data})
+
+(defn- process-search-request
+  [req]
+  (let [queries (-> req :params :q distinct)
+        data (cp/pmap pool search queries)]
+    (json/write-str data)))
+
+(defn- app
+  [req]
+  (assoc
+    response-template :body
+    (process-search-request req)))
 
 (defn -main
   [& args]
   (println "Starting Server!")
-  (http/run-server (wrap-defaults app api-defaults)
-                   {:port 8080}))
+  (http/run-server (rm/wrap-defaults app rm/api-defaults)
+    {:port 8080}))
